bbe1376599f574dac72b172ce24d5a8172281871
Tosin Ayoola <tosinayoola@gmail.com>
2025-08-23 12:43:01 +0100

    updated settings & yamal file

--- commerce/settings.py
+++ commerce/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['e-mrkt-api.onrender.com', 'localhost', ]
+ALLOWED_HOSTS = ['e-mrkt-api.onrender.com', 'localhost', '127.0.0.1']
 CORS_ALLOWED_ORIGINS = ["http://localhost:4200"]
 
 

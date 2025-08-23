ea1d190c862c660ef56ec9a271285c9deda0cf4f
Tosin Ayoola <tosinayoola@gmail.com>
2025-08-23 14:25:37 +0100

    update for creating superuser

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 apt-get update -qq && apt-get install -y libpq-dev python3-dev gcc g++ libssl-dev
 pip install --no-cache-dir --force-reinstall --upgrade pip
 pip install --no-cache-dir -r requirements.txt
-python manage.py collectstatic --noinput --ignore '*.pyc' --ignore '*.log'  # Ignore more files
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='${SUPERUSER_USERNAME}').delete(); User.objects.create_superuser('${SUPERUSER_USERNAME}', '${SUPERUSER_EMAIL}', '${SUPERUSER_PASSWORD}')" | python manage.py shell
+python manage.py collectstatic --noinput --ignore '*.pyc' --ignore '*.log'

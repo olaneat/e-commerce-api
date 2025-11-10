6a55d47dd0c0b39662aac5ff5f0bca9231b7d877
Tosin Ayoola <tosinayoola@gmail.com>
2025-11-10 13:17:14 +0100

    updated vercel deployment settings

--- commerce/settings/prod.py
+++ commerce/settings/prod.py
@@ -2,8 +2,14 @@
 from .base import *
 import os
 DEBUG = False
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'neat-storez-api-env.eba-zavhb3km.us-west-2.elasticbeanstalk.com', 'https://neatstorez.vercel.app/']
+ALLOWED_HOSTS = [  'https://neatstorez.vercel.app/']
 CORS_ALLOWED_ORIGINS = ['https://neatstorez.vercel.app']
+DATABASES = {
+    'default': dj_database_url.parse(
+        os.environ.get('PROD_DB_URL')
+    )
+}
+DATABASES['default']['CONN_MAX_AGE'] = 600
 
 # Use AWS's database
 # DATABASES = {
@@ -18,12 +24,13 @@ CORS_ALLOWED_ORIGINS = ['https://neatstorez.vercel.app']
 #     }
 # }
 
-db_url = config('PROD_DB_URL')  # Use Render's provided variable
-if not db_url:
-    db_url = config('PROD_DB_URL')  # Fallback for local testing
-if db_url:
-    config = dj_database_url.parse(db_url)
-    config['CONN_MAX_AGE'] = 600  # Set conn_max_age
-    DATABASES['default'] = config
+# db_url = os.con('PROD_DB_URL')  # Use Render's provided variable
+# if not db_url:
+#     db_url = config('PROD_DB_URL')  # Fallback for local testing
+# if db_url:
+#     config = dj_database_url.parse(db_url)
+#     config['CONN_MAX_AGE'] = 600  # Set conn_max_age
+#     DATABASES['default'] = config
 # Static and media files for Render
+STATIC_ROOT = BASE_DIR / 'staticfiles_build'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

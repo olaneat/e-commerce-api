f28284bc76be48d646384086daa5d37f8a52613d
Tosin Ayoola <tosinayoola@gmail.com>
2025-12-06 13:05:26 +0100

    update the cors settings

--- commerce/settings/prod.py
+++ commerce/settings/prod.py
@@ -3,7 +3,7 @@ from .base import *
 import os
 DEBUG = True
 ALLOWED_HOSTS = [  'e-mrkt-api.onrender.com']
-CORS_ALLOWED_ORIGINS = ['https://neatstorez.vercel.app', 'localhost']
+CORS_ALLOWED_ORIGINS = ['https://neatstorez.vercel.app', "https://localhost", "http://localhost"]
 DATABASES = {
     'default': dj_database_url.config(
         default=config("PROD_DB_URL"),

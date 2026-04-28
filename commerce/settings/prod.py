0ee273d2142b949e1b3de8a46e5437141ff66ff5
Tosin Ayoola <tosinayoola@gmail.com>
2026-04-28 22:57:43 +0100

    update

--- commerce/settings/prod.py
+++ commerce/settings/prod.py
@@ -2,7 +2,7 @@
 from .base import *
 import os
 DEBUG = True
-ALLOWED_HOSTS = [  'e-mrkt-api.onrender.com', 'localhost']
+ALLOWED_HOSTS = [  'e-mrkt-api.onrender.com', 'localhost', "http://localhost:5173"]
 CORS_ALLOWED_ORIGINS = ['https://neatstorez.vercel.app', "http://localhost","http://localhost:5173"]
 DATABASES = {
     'default': dj_database_url.config(
